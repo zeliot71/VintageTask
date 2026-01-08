33a7a1c31bbb689cd2062b835b7c1ed403d20848
zeliot71 <zihadsha701@gmail.com>
2026-01-08 06:59:43 +0600

    project Structure and database initials

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="initial-scale=1, width=device-width">
+  	
+  	<link rel="stylesheet"  href="./assets/css/index.css" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=[object Object]&display=swap" />
+  	
+  	
+  	
+</head>
+<body>
+  	
+  	<div class="landing">
+    		<img class="image-1-icon" src="assets/images/image 1.png" alt="">
+    		
+    		<img class="image-2-icon" src="assets/images/image 2.png" alt="">
+    		
+    		<div class="image-3-parent">
+      			<img class="image-3-icon" src="assets/images/logo.png" alt="">
+      			
+      			<div class="to-smooth-your">To smooth your Group  Work Experience</div>
+      			<img class="button-icon" alt="">
+      			
+    		</div>
+  	</div>
+  	
+  	
+  	
+  	
+</body>
+</html>
